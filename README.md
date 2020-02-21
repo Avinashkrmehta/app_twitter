08b08987cae482e5e5fa33b687c858514135cbec
avi4791 <avi4791@gmail.com>
2020-02-21 07:28:16 +0530

    authentication and tweets done

--- README.md
+++ README.md
@@ -22,3 +22,15 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+avinash@avinash:~/Desktop$ ruby -v
+ruby 2.6.3p62 (2019-04-16 revision 67580) [x86_64-linux]
+avinash@avinash:~/Desktop$ rails -v
+Rails 6.0.2.1
+
+
+
+avinash@avinash:~/Desktop/app_twitter$ ruby -v
+ruby 2.6.3p62 (2019-04-16 revision 67580) [x86_64-linux]
+avinash@avinash:~/Desktop/app_twitter$ rails -v
+Rails 6.0.2.1
+avinash@avinash:~/Desktop/app_twitter$ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # devise_for :users, :controllers => { registrations: 'registrations' }
+  # resources :tweeets
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+        delete "sign_out", to: "sessions#destroy"
+      end
+    end
+  end
+  resources :tweeets
+  root "tweeets#index"
 end
